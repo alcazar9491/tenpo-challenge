cb6e0fb32e4c5b8db6440cf64c5ccda4d4350ac2
Jorge Alcazar Diaz <80380017+alcazar9491@users.noreply.github.com>
2025-06-09 02:51:04 -0700

    hotfix deleted files

--- src/adapters/character.adapter.ts
+++
@@ -1,5 +0,0 @@
-import type { Character } from "models/character.model";
-
-export const createCharacterAdapter = (characterList : Character[]) => characterList.map( character => ({
-    name: character.name
-}))
