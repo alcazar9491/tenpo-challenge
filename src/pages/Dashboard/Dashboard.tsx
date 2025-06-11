e2756e11655c31beaca5873c94aceed8d6b28479
Jorge Alcazar Diaz <80380017+alcazar9491@users.noreply.github.com>
2025-06-10 17:31:43 -0700

    Update Dashboard.tsx

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,30 +1,35 @@
-import { useMemo, useState } from "react";
-
+import { useState, type ReactElement } from "react";
 import { SelectApi } from "./components/SelectApi";
 import { ApiOption } from "./types";
 import { Pokemons } from "./components/Pokemons/Pokemons";
 import { Swaps } from "./components/Swaps/Swaps";
 
+
+const API_COMPONENT_MAP: Record<Exclude<ApiOption, ApiOption.empty>, ReactElement> = {
+  [ApiOption.pokemon]: <Pokemons />,
+  [ApiOption.swaps]: <Swaps />,
+};
+
 const Dashboard = () => {
+  const [selectedApi, setSelectedApi] = useState<ApiOption>(ApiOption.empty);
 
-  const [SelectedApi, setSelectedApi] = useState<ApiOption>(ApiOption.empty);
+  const renderContent = () => {
+    if (selectedApi === ApiOption.empty) {
+      return (
+        <div className="w-full text-center dark:text-white p-8">
+          <p className="text-lg">Please select an API to continue</p>
+        </div>
+      );
+    }
 
-  const ApiComponentMap = useMemo(
-    () => ({
-      pokemon: <Pokemons />,
-
-        swaps: <Swaps />,
-    }),
-    []
-  );
+    const Component = API_COMPONENT_MAP[selectedApi as Exclude<ApiOption, ApiOption.empty>];
+    return Component || null;
+  };
 
   return (
-    <div className="w-full flex flex-wrap justify-center ">
-      <SelectApi selectedApi={SelectedApi} setSelectedApi={setSelectedApi} />
-
-      {
-        SelectedApi !== "" ? ApiComponentMap[SelectedApi] : <div className="w-full text-center dark:text-white "> Please Selectet an Api to continue</div>
-      }
+    <div className="w-full flex flex-wrap justify-center">
+      <SelectApi selectedApi={selectedApi} setSelectedApi={setSelectedApi} />
+      {renderContent()}
     </div>
   );
 };
